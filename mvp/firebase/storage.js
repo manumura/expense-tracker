ad17616894d3404d8753077bc94b16721da514da
Emmanuel Mura <emmanuel.mura@gmail.com>
2022-12-03 17:26:20 +0700

    storage url

--- mvp/firebase/storage.js
+++ mvp/firebase/storage.js
@@ -20,7 +20,7 @@ import { deleteObject, getDownloadURL as getStorageDownloadURL, ref, uploadBytes
 import { storage } from './firebase';
  
 // Bucket URL from Storage in Firebase Console
-const BUCKET_URL = "";
+const BUCKET_URL = "gs://nextjs-expense-tracker.appspot.com";
  
 // Uploads image and returns the storage bucket
 export async function uploadImage(image, uid) {
